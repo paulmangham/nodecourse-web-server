061c17313bb29c717f2527b59233947d4c4fd34c
Paul Mangham ML <MissionLabs@MachineName.local>
2020-08-04 12:01:51 +0100

    Update app.js to get port from env var

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = '';
     const location = search.value;
 
-    fetch('http://127.0.0.1:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = 'Error: ' + data.error;
